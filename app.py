bc7dee89bf974349b0a1d4c71e7255761235248d
Olalekan Babawale <padinality@yahoo.com>
2019-11-15 13:07:54 +0100

    Add migrations

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
+# from models import Result
 import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-print("*****", os.environ['APP_SETTINGS'])
+from models import Result
 
 
 @app.route('/')
--- config.py
+++ config.py
@@ -8,6 +8,7 @@ class Config(object):
     DEVELOPMENT = False
     SECRET_KEY = "\xf9\x8a\x08zH\xc4\r\xee\xd4\xe1\xfd\xb2@\xdd1["
     CSRF_ENABLED = True
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
 class ProductionConfig(Config):
